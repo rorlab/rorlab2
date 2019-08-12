1a5c7a385a5d820651691287a3c51e8f846a8da2
Lucius Choi <lucius.choi@gmail.com>
2019-08-13 01:58:14 +0900

    updated devise/registrations/edit view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def user_profile(user, options={ width: '20px', class: 'rounded-circle'})
+    user.email
+    # user_profile_url = user.user_profile.nil? ? Gravatar.new(user.email).image_url : user.user_profile.avatar_url(:thumb)
+    # image_tag(user_profile_url, width: options[:width], class: options[:class], style: 'background-color:#eaeaea;')
+  end
 end
