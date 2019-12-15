fef1737f5c97509f8fa2241e638d34cace2feb2c
Lucius Choi <lucius.choi@gmail.com>
2019-12-15 12:32:14 +0900

    added logo image to sweetalert2

--- app/javascript/sweetalert/index.js
+++ app/javascript/sweetalert/index.js
@@ -4,8 +4,8 @@ import Rails from '@rails/ujs'
 Rails.confirm = function (message, element) {
   const swalWithBootstrap = swal.mixin({
     customClass: {
-      confirmButton: 'btn btn-success mr-2',
-      cancelButton: 'btn btn-info'
+      confirmButton: 'btn btn-success w-25 mr-2',
+      cancelButton: 'btn btn-info w-25'
     },
     buttonsStyling: false
   })
@@ -14,7 +14,10 @@ Rails.confirm = function (message, element) {
 
   swalWithBootstrap.fire({
       html: message,
-      type: 'success',
+      // type: 'success',
+      imageUrl: '/assets/rorlab_320_no_border.png', 
+      imageHeight: 76,
+      imageWidth: 150,
       showCancelButton: true,
       confirmButtonText: i18n.t('okay'),
       cancelButtonText: i18n.t('cancel')
