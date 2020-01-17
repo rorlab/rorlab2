5cb7f36242b5aa49e73bd8c855afb9f34453f3ed
Lucius Choi <lucius.choi@gmail.com>
2020-01-18 02:52:23 +0900

    enabled message including image to be sent

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :rcable
+
+  has_one_attached :image
+  
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 
   validates :content, presence: { message: '메시지 창이 비어 있습니다.'}
